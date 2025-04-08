96124839cfd99a347f5e4e63f1634de052c101dc
Peter Svensson <peter@sparetimecoders.com>
2025-04-08 15:19:42 +0200

    refactor: update GitHub checks for CI modules
    Redefine GitHub checks in locals to simplify integration. Replace 
    previous check structures with individual check mappings for build, 
    commitlint, and pre-commit validations. Adjust all CI modules to 
    reference updated local variable for checks, improving consistency 
    and maintainability across the configurations.

--- repositories/locals.tf
+++ repositories/locals.tf
@@ -1,12 +1,28 @@
 locals {
-  github_checks = [
+  build_check = {
+    context : "build",
+    integration_id : 15368
+  }
+  commitlint_check = {
+    context : "commitlint",
+    integration_id : 15368
+  }
+  precommit_check = {
+    context : "pre-commit",
+    integration_id : 15368
+  }
+  bk_plugin_checks = [
     {
-      context : "commitlint",
-      integration_id : 15368
-    },
-    {
-      context : "pre-commit",
-      integration_id : 15368
+      context        = "plugin-linter"
+      integration_id = 15368
+      }, {
+      context        = "plugin-tester"
+      integration_id = 15368
     }
   ]
+  default_github_checks = [
+    local.commitlint_check,
+    local.precommit_check
+  ]
+  tf_github_checks = concat(local.default_github_checks, [local.build_check])
 }
